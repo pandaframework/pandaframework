7bfe12993cc34f3fffb57550f903e9e2ac6b6de8
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-11-03 16:50:36 +0800

    Add tests for aspects

--- polymorphic-ecs/src/main/kotlin/io/polymorphicpanda/polymorphic/ecs/aspect.kt
+++ polymorphic-ecs/src/main/kotlin/io/polymorphicpanda/polymorphic/ecs/aspect.kt
@@ -1,21 +1,9 @@
 package io.polymorphicpanda.polymorphic.ecs
 
-class Aspect internal constructor(internal val included: List<ComponentType>,
-                                  internal val excluded: List<ComponentType>) {
-
-    fun with(vararg componentTypes: ComponentType): Aspect {
-        return Aspect(
-            included + componentTypes,
-            excluded
-        )
-    }
-
-    fun without(vararg componentTypes: ComponentType): Aspect {
-        return Aspect(
-            included,
-            excluded + componentTypes
-        )
-    }
+data class Aspect internal constructor(internal val included: List<ComponentType>,
+                                       internal val excluded: List<ComponentType>) {
+    fun with(vararg componentTypes: ComponentType) = copy(included = included + componentTypes)
+    fun without(vararg componentTypes: ComponentType) = copy(excluded = excluded + componentTypes)
 }
 
 private val initial = Aspect(emptyList(), emptyList())
