b1afb7df81f1bb035a42ff8ae089e606f694ecc3
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2017-11-03 22:22:50 +0800

    Add tests for EntityEditor

--- polymorphic-ecs/src/test/kotlin/org/polymorphicpanda/polymorphic/ecs/ComponentMapperSpec.kt
+++ polymorphic-ecs/src/test/kotlin/org/polymorphicpanda/polymorphic/ecs/ComponentMapperSpec.kt
@@ -4,17 +4,17 @@ import com.natpryce.hamkrest.assertion.assert
 import com.natpryce.hamkrest.equalTo
 import io.polymorphicpanda.polymorphic.ecs.Component
 import io.polymorphicpanda.polymorphic.ecs.ComponentMapper
+import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.subject.SubjectSpek
 
-internal object ComponentMapperSpec: SubjectSpek<ComponentMapper>({
-    val mapper by subject { ComponentMapper() }
-
+internal object ComponentMapperSpec: Spek({
     class C1: Component
     class C2: Component
 
-    describe("ComponentMapper#map") {
+    val mapper by memoized { ComponentMapper() }
+
+    describe("ComponentMapper#map(...)") {
         it("produces unique ids") {
             assert.that(mapper.map(C1::class), !equalTo(mapper.map(C2::class)))
         }
